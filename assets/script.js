58ceaf4de54cdbc52a1bed4f2d842238c947e76e
wisner.maggiel@gmail.com <wisners@Wisnerss-iMac.local>
2021-11-13 12:24:26 -0800

    pushed current weather and appended current weather div. Not formatted. Need to run 5 day forecast and save search to local storage

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,16 @@ var todaysWeather = $('#todays-weather');
 var forecastDiv = $('#forecast-weather');
 var cityInput= $('#city-input');
 var searchedCityName = $('#city-name');
+var currentWeather = $('#current-weather-list');
+
+//empty array for weather conditions
+var currentWeatherArr = [];
+
+//key for weather api
+var APIkey= "4a86dc884cb9d21f0d6c42e65cb6a739";
+
+//search count for searched city list
+var searchCount = 0;
 
 //luxon date/time
 let DateTime = luxon.DateTime;
@@ -18,19 +28,31 @@ function timeTracker() {
     console.log(today.toLocaleString());
 };
 
-var APIkey= "4a86dc884cb9d21f0d6c42e65cb6a739";
-
-//search count for searched city list
-var searchCount = 0;
 
 //store search data in searched city list
 //TODO: fix local storage
-for (i=0; i < localStorage.length; i++) {
-    var cityName = localStorage.getItem(i);
-    searchResultsList.append("<li class = 'search-item mt-2 col-12'> + cityName + '</li'");
-    
-};
+// for (i=0; i < localStorage.length; i++) {
+//     var cityName = localStorage.getItem(i);
+//     searchResultsList.append("<li class = 'search-item mt-2 col-12'> + cityName + '</li'");
 
+function displayCurrentWeather (data) {
+        var cityName = ('Showing results for ' + data.name);
+        var temp = ('Current Temperature: ' + data.main.temp);
+        var wind = ('Wind Speeds: ' + data.wind.speed +' mph');
+        var minTemp = ('Low Temperature: ' + data.main.temp_min + ' degrees');
+        var maxTemp =  ('High Temperature: ' + data.main.temp_max + ' degrees');
+        var vis = ('Visibility: ' +data.main.visibility + '%');
+        var icon = data.weather[0].icon;
+        var hum = ('Humidity: ' + data.main.humidity + '%');
+        var currentWeatherArr= [cityName,temp,wind,minTemp,maxTemp,vis,icon,hum];
+        currentWeatherArr.push(currentWeatherArr);
+        currentWeatherDiv = document.createElement('div');
+        currentWeatherDiv.append(currentWeatherArr);
+        todaysWeather.append(currentWeatherDiv);
+        console.log(currentWeatherArr);
+
+//TODO: create 5 day forecast & display in new divs/cards
+        };
 //on click search button
 searchBtn.on('click', function(event) {
     event.preventDefault();
@@ -40,34 +62,14 @@ searchBtn.on('click', function(event) {
 
     if(cityName) {
         getWeather(cityName);
-        cityInput.value = "";
-        cityName.localStorage.setItem(cityName)
+        // cityInput.value = "";
+    //TODO: Set city to local storage
     } else {
         alert("Please enter a city name");
     }
 });
 
-var displayWeather = function(data) {
 
-    fetch(currentWeatherUrl)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        var cityName = data.name;
-        var curHum = data.main.humidity;
-        var curTemp = data.main.temp;
-        var curMinTemp = data.main.temp_min;
-        var curMaxTemp = data.main.temp_max;
-        var curVis = data.sys.visibility;
-        var curWind = data.wind.speed
-        var curIcon = data.weather[0].icon;
-
-//TODO: Append this data to current-weather-list
-
-    });
-};
 var getWeather = function (city) {
 
     currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey + "&units=imperial";
@@ -78,30 +80,15 @@ var getWeather = function (city) {
     todaysWeather.empty();
     forecastDiv.empty();
 
-    displayWeather();
+    fetch(currentWeatherUrl)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        displayCurrentWeather(data);  
+    });
 };
-
-
-        // response.json().then(function(data) {
-        //     displayWeather(data);
-        //     console.log(data);
-        // });
-    // });
-    // var searchStorage = localStorage.setItem(searchCount, data.name);
-    // searchCount = searchCount +1;
-
-
-// var displayWeather = function(data) {
-//     console.log(data.name);
-
-//     // todaysWeather.append("<h2 id='city-name'> + data.name + '("+ today +") " + "<img src='https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png'></h2>");
-
-//     var searchStorage = localStorage.setItem(searchCount, data.name);
-//     searchCount = searchCount +1;
-// };
-
-
-
 
 timeTracker();
 
